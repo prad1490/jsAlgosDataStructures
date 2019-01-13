548b7eda8dc5927b62fad2821cb09417a9c0208d
prad1490 <dheep2lrn@gmail.com>
2019-01-13 15:29:47 +0530

    added is Sub sequence logic

--- sumZero.js
+++ sumZero.js
@@ -1,5 +1,5 @@
 //Naive Solution, O(n2)//n aquare
-/* function sumZero(array) {
+function sumZero(array) {
   let getArray = array.sort();
   for (let i = 0; i < getArray.length; i++) {
     for (let j = i + 1; j < getArray.length; j++) {
@@ -8,11 +8,11 @@
       }
     }
   }
-} */
+}
 
 //better Soln below O(N) space O(1)
 
-function sumZero(array) {
+function Nform(array) {
   array.sort();
   let left = 0;
   let right = array.length - 1;
@@ -26,4 +26,4 @@ function sumZero(array) {
     }
   }
 }
-sumZero([5, 0, 3, 1, 4, 6, -4]);
+Nform([5, 0, 3, 1, 4, 6, -4]);
